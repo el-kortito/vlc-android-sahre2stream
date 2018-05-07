fd4896861f5b8360e3115db7ab83d6bbb604375f
itai <el.kortito@gmail.com>
2018-05-07 19:55:02 +0300

    added set/resetPreferences

--- vlc-android/src/org/videolan/vlc/gui/ShareToStreamActivity.java
+++ vlc-android/src/org/videolan/vlc/gui/ShareToStreamActivity.java
@@ -1,26 +1,52 @@
 package org.videolan.vlc.gui;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 import org.videolan.medialibrary.media.MediaWrapper;
 import org.videolan.vlc.VLCApplication;
 import org.videolan.vlc.media.MediaUtils;
 
 public class ShareToStreamActivity extends BaseActivity {
+    String videoSwitchPref;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        String url = intent.getStringExtra(Intent.EXTRA_TEXT);
+        setPreferences();
+        String url = getIntent().getStringExtra(Intent.EXTRA_TEXT);
         setupStream(url);
         finish();
     }
 
-    private void setupStream(String url) {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        resetPreferences();
+    }
+
+    private void setPreferences() {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        videoSwitchPref = pref.getString("video_action_switch", null);
+        pref.edit().putString("video_action_switch", "1").apply();
+    }
+
+    private void resetPreferences() {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        if (videoSwitchPref == null) {
+            pref.edit().remove("video_action_switch").apply();
+        } else {
+            pref.edit().putString("video_action_switch", videoSwitchPref).apply();
+        }
+    }
+
+    private void setupStream (String url){
         final MediaWrapper mw = new MediaWrapper(Uri.parse(url));
         mw.setType(MediaWrapper.TYPE_STREAM);
         MediaUtils.openMedia(VLCApplication.getAppContext(), mw);
     }
 }
-
